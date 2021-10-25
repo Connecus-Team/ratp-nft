5abefda0d34397317887a43696bba6b5883d924e
= <hopekr@dongguk.edu>
2021-10-25 14:36:08 +0900

    add class active

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useLocation} from 'react-router-dom';
 import './styles.scss';
 
 function Heading() {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const splitLocation = pathname.split('/');
+  console.log(splitLocation);
   return (
     <div className="heading">
       <ul>
-        <li><NavLink to="/">Trang Chủ</NavLink></li>
-        <li><NavLink to="/work">Hoạt Động</NavLink></li>
-        <li><NavLink to="/future"> Kế Hoạch</NavLink></li>
-        <li><NavLink to="/event">Sự Kiện</NavLink></li>
-        <li><NavLink to="/member">Thành Viên</NavLink></li>
+        <li><NavLink className={splitLocation[1] === '' ? 'page-active' : ''} to="/">Trang Chủ</NavLink></li>
+        <li><NavLink className={splitLocation[1] === 'work' ? 'page-active' : ''} to="/work">Hoạt Động</NavLink></li>
+        <li><NavLink className={splitLocation[1] === 'future' ? 'page-active' : ''} to="/future"> Kế Hoạch</NavLink></li>
+        <li><NavLink className={splitLocation[1] === 'event' ? 'page-active' : ''}to="/event">Sự Kiện</NavLink></li>
+        <li><NavLink className={splitLocation[1] === 'member' ? 'page-active' : ''}to="/member">Thành Viên</NavLink></li>
         <li><a href="https://github.com/huonghope/ratp-nft" target="_blank" rel="noreferrer">Mã Nguồn</a></li>
       </ul>
     </div>
